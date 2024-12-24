ec5f79ad6e6ea041af8a62953e5447ba3268c563
JYOTIRMOY BANERJEE <144256407+jyotirmoybanerjee2222@users.noreply.github.com>
2024-12-24 13:58:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fighting the darkness/fighting-the-darkness.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int maxDays(vector<int> &arr) {
+        // code here
+        int max = 0;
+        for(int i = 0;i<arr.size();i++)
+        {
+            if(arr[i]>max)
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxDays(arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
